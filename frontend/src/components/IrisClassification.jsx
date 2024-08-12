d16cf2a8b9cd46d5eef6e1844b2e08f85d9f0618
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-08-12 19:41:57 +0200

    frontend deployment

--- frontend/src/components/IrisClassification.jsx
+++ frontend/src/components/IrisClassification.jsx
@@ -29,7 +29,7 @@ const IrisClassification = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://127.0.0.1:8000/predict`, {
+      const response = await fetch(`https://iris-species-classification-1.onrender.com/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
